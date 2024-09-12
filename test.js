df405fd11a0d861293e051b4d93c8cbfec0d047d
Franklin Ruan <Franklinruan.b@gmail.com>
2024-09-12 19:14:38 +1000

    Removed unnecessary test file

--- test.js
+++
@@ -1,8 +0,0 @@
-let date = new Date();
-console.log(date);
-console.log(date.getDate());
-const test = [12,23,3];
-const test2 = [12,23,3];
-console.log(test == test2);
-const __dirname = import.meta.dirname;
-console.log(import.meta);
